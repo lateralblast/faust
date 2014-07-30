b5c92d88c68499f334899b44dd79e5c3f79cf019
Richard Spindler <richard@lateralblast.com.au>
2014-07-30 13:46:50 +1000

    Added rc type for rc.conf

--- faust.rb
+++ faust.rb
@@ -1,5 +1,5 @@
 # Name:         faust (Facter Automatic UNIX Symbolic Template)
-# Version:      1.2.2
+# Version:      1.2.3
 # Release:      1
 # License:      CC-BA (Creative Commons By Attrbution)
 #               http://creativecommons.org/licenses/by/4.0/legalcode
@@ -724,6 +724,8 @@ def handle_configfile(kernel,type,file_info,os_distro,os_version)
     prefix = type.gsub(/configfile/,"")
   end
   case prefix
+  when "rc"
+    file = "/etc/rc.conf"
   when "ftpd"
     file = "/etc/proftpd.conf"
   when "ftpdaccess"
@@ -1943,7 +1945,7 @@ if file_name !~ /template|operatingsystemupdate/ and get_fact == "yes"
           fact = handle_sudo(kernel,modname,type,file_info,os_distro,os_version)
         when "ftpd"
           fact = handle_ftpd(kernel,modname,type,file_info,os_distro,os_version)
-        when /ssh$|krb5$|hostsallow$|hostsdeny$|snmp$|sendmail$|ntp$|aliases$|grub$|selinux$|cups$|apache$|modprobe|network|xscreensaver|ftpaccess$|proftpd$|vsftpd$|gdmbanner$|gdm$|gdminit$|sysstat$/
+        when /ssh$|krb5$|hostsallow$|hostsdeny$|snmp$|sendmail$|ntp$|aliases$|grub$|selinux$|cups$|apache$|modprobe|network|xscreensaver|ftpaccess$|proftpd$|vsftpd$|gdmbanner$|gdm$|gdminit$|sysstat$|^rc$/
           fact = get_param_value(kernel,modname,type,file_info,os_distro,os_version)
         when "groupexists"
           fact = handle_groupexists(file_info)
